6660ab28170761d5c90139ca6d10a27e3397fcda
jabber <2898117012@qq.com>
2023-09-05 19:50:04 +0800

    init

--- se-elementor.php
+++ se-elementor.php
@@ -16,3 +16,28 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+if ( ! function_exists( 'se_elementor_activation' ) ) {
+	function se_elementor_activation() {
+
+	}
+}
+
+register_activation_hook( 'se_elementor_activation' );
+
+if ( ! function_exists( 'se_elementor_deactivation' ) ) {
+	function se_elementor_deactivation() {
+
+	}
+}
+
+register_deactivation_hook( 'se_elementor_deactivation' );
+
+if ( ! function_exists( 'se_elementor_load_textdomain' ) ) {
+	function se_elementor_load_textdomain() {
+		load_plugin_textdomain( 'se_elementor', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+}
+
+add_action( 'init', 'se_elementor_load_textdomain' );
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shopeo/se-elementor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '56cf767c1e7cd65514e0e6c0385608d1b15ba536',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shopeo/se-elementor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '56cf767c1e7cd65514e0e6c0385608d1b15ba536',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
